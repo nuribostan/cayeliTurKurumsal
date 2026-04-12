7834cae45f1a39f3272c7981104cbdff4ff76ace
Nuri Bostan <nuribostan@users.noreply.github.com>
2026-04-12 17:32:03 +0300

    revizeler v4

--- components/TurDetay/Hero.tsx
+++ components/TurDetay/Hero.tsx
@@ -53,8 +53,8 @@ const TurDetayHero = ({ tur }: TurProgramiProps) => {
         />
         <div className="absolute inset-0 bg-black/30 lg:bg-black/10"></div>
 
-        <div className="absolute top-1/6 left-[5%] lg:left-[7.5%] z-10 w-[90%]">
-          <h1 className="text-shadow-lg text-4xl md:text-5xl lg:text-6xl text-white font-bold flex flex-col lg:flex-row items-start lg:items-end gap-2 lg:gap-4 drop-shadow-lg lg:drop-shadow-none">
+        <div className="absolute top-1/7 sm:top-1/4 left-[5%] lg:left-[7.5%] z-10 w-[90%]">
+          <h1 className="text-shadow-lg text-shadow-black  text-4xl md:text-5xl lg:text-6xl text-white font-bold flex flex-col lg:flex-row items-start lg:items-end gap-2 lg:gap-4 drop-shadow-lg lg:drop-shadow-none">
             <span>{tur?.turName}</span>
             <span className="font-medium text-white text-2xl md:text-3xl lg:text-4xl">
               {tur?.turType}
@@ -63,7 +63,7 @@ const TurDetayHero = ({ tur }: TurProgramiProps) => {
         </div>
 
         <div className="absolute top-1/2 lg:top-1/3 mt-0 lg:mt-10 left-[5%] lg:left-[7.5%] flex flex-col sm:flex-row items-start sm:items-center gap-4 z-10">
-          <div className="bg-white/95 backdrop-blur-sm px-3 py-2 sm:py-3 rounded-xl flex items-center gap-3 shadow-lg min-w-48">
+          <div className="bg-white/95 backdrop-blur-sm px-3 py-2 sm:py-3 rounded-xl flex items-center gap-3 shadow-lg min-w-38">
             <Image
               src={pin}
               alt="pin"
@@ -75,7 +75,7 @@ const TurDetayHero = ({ tur }: TurProgramiProps) => {
               {tur?.location}
             </h4>
           </div>
-          <div className="bg-white/95 backdrop-blur-sm px-3 py-2 sm:py-3 rounded-xl flex items-center gap-3 shadow-lg min-w-48">
+          <div className="bg-white/95 backdrop-blur-sm px-3 py-2 sm:py-3 rounded-xl flex items-center gap-3 shadow-lg min-w-38">
             <Image
               src={bus}
               alt="bus"
@@ -88,7 +88,7 @@ const TurDetayHero = ({ tur }: TurProgramiProps) => {
             </h4>
           </div>
 
-          <div className="bg-white/95 backdrop-blur-sm px-3 py-2 sm:py-3 rounded-xl flex items-center gap-3 shadow-lg min-w-48">
+          <div className="bg-white/95 backdrop-blur-sm px-3 py-2 sm:py-3 rounded-xl flex items-center gap-3 shadow-lg min-w-38">
             <Image
               src={tl}
               alt="tl"
